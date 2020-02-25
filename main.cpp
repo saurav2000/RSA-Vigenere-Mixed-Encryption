1b54e0a2105f6ae14c3df6705c7335346ae202c3
Saurav <musunurusaurav@gmail.com>
2020-02-26 01:35:50 +0530

    Added RSA and vigenere cryptions and file handling

--- main.cpp
+++ main.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <string>
 #include <gmpxx.h>
 
 using namespace std;
 
 #define big_int mpz_class
+
+map<char, int> char_to_int;
+map<int, char> int_to_char;
+
+void map_init()
+{
+	for(int i=97;i<123;++i)
+	{
+		char ch = (char)i;
+		char_to_int[ch] = i-97;
+		int_to_char[i-97] = ch;
+	}
+	//space
+	char_to_int[' '] = 26;
+	int_to_char[26] = ' ';
+	//fullstop
+	char_to_int['.'] = 27;
+	int_to_char[27] = '.';
+	//comma
+	char_to_int[','] = 28;
+	int_to_char[28] = ',';
+	//apostrophe
+	char_to_int['\''] = 29;
+	int_to_char[29] = '\'';
+
+	for(int i=48;i<58;++i)
+	{
+		char ch = (char)i;
+		char_to_int[ch] = i-18;
+		int_to_char[i-18] = ch;	
+	}
+}
 
 big_int ext_gcd(big_int a, big_int b, big_int *x, big_int *y)
 {
@@ -25,7 +59,7 @@ big_int mod_inv(big_int a, big_int n)
 {
 	big_int x, y;
 	ext_gcd(a, n, &x, &y);
-	return x;
+	return ((x%n)+n)%n;
 }
 
 big_int mod_exp(big_int a, big_int b, big_int n)
@@ -123,10 +157,89 @@ void generate_key(gmp_randclass &rand_gen, big_int bit_cnt, char *public_file, c
 	fout.close();
 }
 
+string RSA_encrypt(string input, big_int e, big_int N)
+{
+	big_int M = 0, mult = 1;
+	for(int i=0;i<input.length();++i)
+	{
+		M+= (char_to_int[input[i]] * mult);
+		mult = mult*40;
+	}
+
+	big_int C = mod_exp(M, e, N);
+	string encrypted = "";
+	while(C!=0)
+	{
+		big_int x = C%40;
+		encrypted+= int_to_char[x.get_si()];
+		C = C/40;
+	}
+
+	return encrypted;
+}
+
+string RSA_decrypt(string input, big_int d, big_int N, big_int p, big_int q)
+{
+	big_int C = 0, mult = 1;
+	for(int i=0;i<input.length();++i)
+	{
+		C+= (char_to_int[input[i]] * mult);
+		mult = mult*40;
+	}
+	big_int M = mod_exp(C, d, N);
+	string decrypted = "";
+	while(M!=0)
+	{
+		big_int x = M%40;
+		decrypted+= int_to_char[x.get_si()];
+		M = M/40;
+	}
+
+	return decrypted;
+}
+
+void RSA_encrypt(char *key_file, char *text_file, char *output_file)
+{
+	ifstream fin;
+	fin.open(key_file);
+	big_int e, N;
+	fin>>e>>N;
+	fin.close();
+
+	fin.open(text_file);
+	string res = "", line ;
+	while(getline(fin, line))
+		res+= (line+" ");
+	fin.close();
+
+
+	ofstream fout(output_file);
+	// fout<<encrypted<<"\n";
+	fout.close();
+}
+
+string vigenere(string a, string key, bool encrypt)
+{
+	string res = "";
+	int n = key.length();
+	for(int i=0;i<a.length();++i)
+	{
+		if(encrypt)
+			res+= int_to_char[((char_to_int[a[i]]+char_to_int[key[i%n]])%40)];
+		else
+			res+= int_to_char[((char_to_int[a[i]]-char_to_int[key[i%n]]+40)%40)];
+	}
+	return res;
+}
+
+
+
 int main(int argc, char const *argv[])
 {
 	gmp_randclass rand_gen(gmp_randinit_mt);
 	rand_gen.seed(time(NULL));
-	cout<<generate_strong_prime(rand_gen, 128)<<"\n";
+	map_init();
+
+
 	return 0;
 }
