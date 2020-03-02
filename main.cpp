5edb92990f35a087a6a91c7ce73681f714a686c0
Saurav <musunurusaurav@gmail.com>
2020-03-03 04:00:45 +0530

    Tester for CA and client

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "CA.h"
 #include "client.h"
-
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
+	// generate_key(128, "A_pub", "A_priv");
+	// generate_key(128, "B_pub", "B_priv");
+	// encrypt("encr", "text", "encrypted");
+	// decrypt("decr", "encrypted", "decrypted");
+	// string s = "india is my country. all indians are my brother's and sister's, but this is 2019.  ";
+	// cout<<vigenere(vigenere(s, "h.e'llo20", true), "h.e'llo20", false)<<"\n";
+
 	return 0;
 }
