16ad42456265a2c0b795061a8c4532ed6b1369ac
Saurav <musunurusaurav@gmail.com>
2020-03-04 23:15:06 +0530

    Changed file input output params

--- client.h
+++ client.h
@@ -1,4 +1,4 @@
 #include "global.h"
 
-void encrypt(char *key_file, char *text_file, char *output_file);
-void decrypt(char *key_file, char *text_file, char *output_file);
+void encrypt(char *vigenere_key, char *send_priv_key, char *recp_pub_key, char *text_file, char *output_file);
+void decrypt(char *recp_priv_key, char *send_pub_key, char *text_file, char *output_file);
